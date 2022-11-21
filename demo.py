89886934ba02c70063bb5a808917f7fb04549242
Sumanta Das <sumantad827@gmail.com>
2022-11-21 19:19:26 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+# python program kilometer to mile conversion
+
+userinput = int(input("enter kilometer : "))
+
+calculate = userinput*0.621371
+
+print('you enter : ', userinput, "km")
+
+print('mile are  : ', calculate)
